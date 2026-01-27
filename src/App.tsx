5444a386d7654de87a3bf1b696930ef270e58a18
analu <dutranalu@hotmail.com>
2026-01-27 11:43:35 -0300

    feat: refactor App component to use Redux for state management and update Prettier configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
-
 import { GlobalStyle } from './styles'
+
+import type { RootState } from './store'
+import { adicionar } from './store/cartSlice'
+import { useGetProdutosQuery } from './store/api'
 
 export type Produto = {
   id: number
@@ -12,21 +17,21 @@ export type Produto = {
 }
 
 function App() {
-  const [produtos, setProdutos] = useState<Produto[]>([])
-  const [carrinho, setCarrinho] = useState<Produto[]>([])
-  const [favoritos, setFavoritos] = useState<Produto[]>([])
+  // RTK Query (substitui o useEffect + fetch)
+  const { data: produtos = [], isLoading, isError } = useGetProdutosQuery()
 
-  useEffect(() => {
-    fetch('https://api-ebac.vercel.app/api/ebac_sports')
-      .then((res) => res.json())
-      .then((res) => setProdutos(res))
-  }, [])
+  // Redux (substitui useState do carrinho)
+  const dispatch = useDispatch()
+  const carrinho = useSelector((state: RootState) => state.cart.itens)
+
+  // Favoritos pode continuar em useState (não faz parte do requisito do slice)
+  const [favoritos, setFavoritos] = useState<Produto[]>([])
 
   function adicionarAoCarrinho(produto: Produto) {
     if (carrinho.find((p) => p.id === produto.id)) {
       alert('Item já adicionado')
     } else {
-      setCarrinho([...carrinho, produto])
+      dispatch(adicionar(produto))
     }
   }
 
@@ -37,6 +42,14 @@ function App() {
     } else {
       setFavoritos([...favoritos, produto])
     }
+  }
+
+  if (isLoading) {
+    return <p>Carregando...</p>
+  }
+
+  if (isError) {
+    return <p>Erro ao carregar produtos</p>
   }
 
   return (
